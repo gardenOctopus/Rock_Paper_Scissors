13c6899f393ce53986ec0cce37e17104be18360a
octopusGarden <zebraspider@protonmail.com>
2022-10-19 13:48:42 -0400

    Added algorithm to script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+//Title: Rock, Paper, Scissors
+//Variables: Strings = Rock, Paper, Scissors???
+
+//Variable: User Input = Prompt: Choose Rock, Paper, Scissors
+
+//Varaible: Random Computer Input = One of Three Above Variables
+
+//If... Else Statement:
+//User === Rock && Computer === Rock
+//Tie
+//User === Rock && Computer === Paper
+//You Lose
+//User === Rock && Computer === Scissors
+//You Win
+
+//User === Paper && Computer === Paper
+//Tie
+//User === Paper && Computer === Rock
+//You Win
+//User === Paper && Computer === Scissors
+//You Lose
+
+//User === Scissors && Computer === Scissors
+//Tie
+//User === Scissors && Computer === Paper
+//You Win
+//User === Scissors && Computer === Rock
+//You Lose
