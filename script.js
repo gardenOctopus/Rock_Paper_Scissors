9d61d790648dbe537a6b4323bab2e0f206c5575e
octopusGarden <zebraspider@protonmail.com>
2022-10-19 14:44:36 -0400

    Created if else statements to determine wins and losses

--- script.js
+++ script.js
@@ -11,12 +11,21 @@ let computer = Math.floor(Math.random() * 3);
 // 0 is Rock
 //1 is Paper
 //2 is Scissors
+if (computer === 0) {
+    computer = "Rock";
+} else if (computer === 1) {
+    computer = "Paper";
+} else {
+    computer = "Scissors";
+}
+console.log(computer);
 
 //If... Else Statement:
 //User === Rock && Computer === Rock
 //Tie
 //User === Rock && Computer === Paper
 //You Lose
+
 //User === Rock && Computer === Scissors
 //You Win
 
@@ -33,3 +42,25 @@ let computer = Math.floor(Math.random() * 3);
 //You Win
 //User === Scissors && Computer === Rock
 //You Lose
+if (user === "Rock" && computer === "Rock") {
+    console.log("Tie");
+} else if (user === "Rock" && computer === "Paper") {
+    console.log("You Lose!");
+} else if (user === "Rock" && computer === "Scissors") {
+    console.log("You Win!");
+} else if (user === "Paper" && computer === "Paper") {
+    console.log("Tie");
+} else if (user === "Paper" && computer === "Rock") {
+    console.log("You Win!") 
+} else if (user === "Paper" && computer === "Scissors") {
+    console.log("You Lose!")
+} else if (user === "Scissors" && computer === "Scissors") {
+    console.log("Tie");
+} else if (user === "Scissors" && computer === "Paper") {
+    console.log("You Win!");
+} else if (user === "Scissors" && computer === "Rock") {
+    console.log("You Lose!");
+} else {
+    console.log("Please type proper answers... @.@");
+}
+
